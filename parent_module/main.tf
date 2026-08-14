cd239185e1f8e9c2820619dedef7aef965dd1989
appakolekar2025 <appakolekar2025@gmail.com>
2026-08-14 23:32:37 +0530

    adding code

--- parent_module/main.tf
+++ parent_module/main.tf
@@ -9,27 +9,27 @@ module "vnet" {
   vnet   = var.virtual_network
 }
 
-module "snet" {
-  source = "../child_module/azurerm_subnet"
-  snet   = var.subnet
-}
-module "nsg" {
-  source = "../child_module/azurerm_nsg"
-  nsg    = var.network_security_group
-}
-module "nic" {
-  source = "../child_module/azurerm_nic"
-  nic    = var.network_interface
-}
-module "bastion" {
-  source  = "../child_module/azurerm_bastion"
-  bastion = var.bastion
-}
-module "vms" {
-  source = "../child_module/azurerm_virtual_machine"
-  vms    = var.virtual_machine
-}
-module "kv" {
-  source = "../child_module/key_vault"
-  kv     = var.key_vault
-}
+# module "snet" {
+#   source = "../child_module/azurerm_subnet"
+#   snet   = var.subnet
+# }
+# module "nsg" {
+#   source = "../child_module/azurerm_nsg"
+#   nsg    = var.network_security_group
+# }
+# module "nic" {
+#   source = "../child_module/azurerm_nic"
+#   nic    = var.network_interface
+# }
+# module "bastion" {
+#   source  = "../child_module/azurerm_bastion"
+#   bastion = var.bastion
+# }
+# module "vms" {
+#   source = "../child_module/azurerm_virtual_machine"
+#   vms    = var.virtual_machine
+# }
+# module "kv" {
+#   source = "../child_module/key_vault"
+#   kv     = var.key_vault
+# }
