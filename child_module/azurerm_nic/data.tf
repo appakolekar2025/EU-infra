3e2eafe6cc257dc4a5097c926d54d77c2823f6ff
Vikas Belekar <belekarvikas@gmail.com>
2026-08-14 23:43:43 +0530

    Revert "Feature"

--- child_module/azurerm_nic/data.tf
+++
@@ -1,7 +0,0 @@
-data "azurerm_subnet" "snet" {
-  for_each = var.nic
-  name                 = each.value.subnet_name
-  resource_group_name  = each.value.resource_group_name
-  virtual_network_name = each.value.virtual_network_name
-}
-
