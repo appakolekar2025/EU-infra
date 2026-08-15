b1148f8066a871d7e0c728c1e5ff29fdbb84bbca
appakolekar2025 <appakolekar2025@gmail.com>
2026-08-15 23:52:07 +0530

    adding code

--- parent_module/provider.tf
+++ parent_module/provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "tfstate"
+    resource_group_name  = "tfstate-rg"
     storage_account_name = "augstorager4123"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
